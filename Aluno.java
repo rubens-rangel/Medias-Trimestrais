7b86da455c701301af5cd5587d661978b9c6b202
Rubens Rangel <rubens_rangel@hotmail.com>
2021-08-06 13:18:39 -0300

    Update Aluno.java

--- Aluno.java
+++ Aluno.java
@@ -1,5 +1,6 @@
 public class Aluno {
 	public String nome;
+	public String status;
 	public double nota1t;
 	public double nota2t;
 	public double nota3t;
@@ -7,5 +8,11 @@ public class Aluno {
 	public double Media() {
 	double media = nota1t + nota2t + nota3t;
 	return media;
-}
+	}
+	public String Situacao(){
+		  if (this.Media() >= 60.00 ){ status = "Aprovado"; }
+		  if (this.Media() < 50.00){status = "Reprovado";}
+		  if (this.Media() >= 50.00 && this.Media() <= 69.00){ status = "Fara a Avaliacao Final"; }
+		  return status;
+	}	
 }
